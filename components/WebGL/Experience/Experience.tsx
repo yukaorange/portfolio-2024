45b9d19033300cc5be0951c2f05c8545cb763591
yukaorange <takaoka.webdesigner@gmail.com>
2024-11-18 14:28:13 +0900

    Floorの処理が重すぎたため、クラッシュしていたものとみられるので、Floorをオミットした

--- components/WebGL/Experience/Experience.tsx
+++ components/WebGL/Experience/Experience.tsx
@@ -105,7 +105,7 @@ export const Experience = () => {
         rotation={[modelRotation.x, modelRotation.y, modelRotation.z]}
       />
       <Panels textures={loadedTextures} />
-      <Floor color={color} />
+      {/* <Floor color={color} /> */}
     </>
   );
 };
