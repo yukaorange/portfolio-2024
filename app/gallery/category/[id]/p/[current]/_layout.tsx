7f8ce8bc12d666a844c7080bd1062add987f7ccf
yukaorange <takaoka.webdesigner@gmail.com>
2024-12-10 03:55:47 +0900

    一度メタ情報用のlayoutをオミットして確認

--- app/gallery/category/[id]/p/[current]/_layout.tsx
+++ app/gallery/category/[id]/p/[current]/_layout.tsx
@@ -1,23 +1,23 @@
-import { Metadata } from 'next';
+// import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: 'takaoka-portfolio2024 | ギャラリー',
-  description:
-    ' Webサイトの創造的な表現に魅力を感じ、興味を持ったことをきっかけにフロントエンドエンジニアリングの学習を始め、日々技術の向上に励んでいます。',
-  openGraph: {
-    title: 'takaoka-portfolio2024',
-    description:
-      ' Webサイトの創造的な表現に魅力を感じ、興味を持ったことをきっかけにフロントエンドエンジニアリングの学習を始め、日々技術の向上に励んでいます。',
-    url: 'https://portfolio-2024-takaoka.vercel.app/',
-    images: [
-      {
-        url: 'https://portfolio-2024-takaoka.vercel.app/ogp.jpg',
-        alt: 'takaoka-portfolio2024',
-      },
-    ],
-  },
-};
+// export const metadata: Metadata = {
+//   title: 'takaoka-portfolio2024 | ギャラリー',
+//   description:
+//     ' Webサイトの創造的な表現に魅力を感じ、興味を持ったことをきっかけにフロントエンドエンジニアリングの学習を始め、日々技術の向上に励んでいます。',
+//   openGraph: {
+//     title: 'takaoka-portfolio2024',
+//     description:
+//       ' Webサイトの創造的な表現に魅力を感じ、興味を持ったことをきっかけにフロントエンドエンジニアリングの学習を始め、日々技術の向上に励んでいます。',
+//     url: 'https://portfolio-2024-takaoka.vercel.app/',
+//     images: [
+//       {
+//         url: 'https://portfolio-2024-takaoka.vercel.app/ogp.jpg',
+//         alt: 'takaoka-portfolio2024',
+//       },
+//     ],
+//   },
+// };
 
-export default function GalleryLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
+// export default function GalleryLayout({ children }: { children: React.ReactNode }) {
+//   return <>{children}</>;
+// }
